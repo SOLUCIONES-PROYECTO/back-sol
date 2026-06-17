b854a1d01d1de4c8718395838fb90012454b2baa
Anneliese01 <annelismcl04@gmail.com>
2026-06-17 12:10:18 -0500

    avance empleado

---
+++ src/main/java/edu/bodega/yessy/back_sol/repositories/EmpleadoRepository.java
@@ -0,0 +1,16 @@
+package edu.bodega.yessy.back_sol.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.bodega.yessy.back_sol.models.Empleado;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+
+    Optional<Empleado> findByUsuarioSistema(String usuarioSistema);
+
+    boolean existsByUsuarioSistema(String usuarioSistema);
+}
